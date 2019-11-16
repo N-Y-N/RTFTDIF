a42fa310f00e37c244186d1cf8eae3f61c9568ce
Neoteric <neotericdeveloper@gmail.com>
2019-11-16 20:58:07 +0530

    Created Mockp for Reference

--- RTFTDIF.ViewModels/DesignViewModels/DCategoryItemListViewModel.cs
+++ RTFTDIF.ViewModels/DesignViewModels/DCategoryItemListViewModel.cs
@@ -61,6 +61,198 @@ namespace RTFTDIF.ViewModels
                     CategoryName = "Delete in next Week",
                     FilesCount = 566,
                     Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in one Month",
+                    FilesCount = 12,
+                    Size = "17.88 GB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in any time",
+                    FilesCount = 123,
+                    Size = "12 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in one Month",
+                    FilesCount = 12,
+                    Size = "17.88 GB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in any time",
+                    FilesCount = 123,
+                    Size = "12 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in one Month",
+                    FilesCount = 12,
+                    Size = "17.88 GB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in any time",
+                    FilesCount = 123,
+                    Size = "12 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in one Month",
+                    FilesCount = 12,
+                    Size = "17.88 GB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in any time",
+                    FilesCount = 123,
+                    Size = "12 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
+                },
+                new DCategoryItemViewModel()
+                {
+                    CategoryName = "Delete in next Week",
+                    FilesCount = 566,
+                    Size = "487 MB"
                 }
             };
         }
