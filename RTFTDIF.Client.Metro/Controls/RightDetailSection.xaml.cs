feb25d4a957aee9ad72beef860ec307e931f31c2
Neoteric <neotericdeveloper@gmail.com>
2019-11-24 14:12:22 +0530

    High level Metro like UI design completed.

---
+++ RTFTDIF.Client.Metro/Controls/RightDetailSection.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RTFTDIF.Client.Metro.Views
+{
+    /// <summary>
+    /// Interaction logic for RightDetailSection.xaml
+    /// </summary>
+    public partial class RightDetailSection : UserControl
+    {
+        public RightDetailSection()
+        {
+            InitializeComponent();
+        }
+
+        private void GridScroller_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+    }
+}
---
+++ RTFTDIF.ViewModels/DesignVMs/DDetailViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTFTDIF.ViewModels
+{
+    public class DDetailViewModel : DetailViewModel 
+    {
+        public static DDetailViewModel Instance => new DDetailViewModel();
+
+        public DDetailViewModel()
+        {
+            Items = new List<ItemViewModel>();
+            Random r = new Random();
+            for (int i = 1; i <= 100; i++)
+            {
+                Items.Add(
+                    new DItemViewModel()
+                    {
+                        SNo = i,
+                        File = $"File00000000000 - {i}",
+                        Path = i % 10 == 0 ? "D:/Dummy/Path/To/This/File/Is/Little/Soooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo/Long" : "D:/Dummy/Path/To/This/File",
+                        Type = ".mp3",
+                        Size = r.Next() + " MB"
+                    }
+                    );
+            }
+        }
+    }
+}
